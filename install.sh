e0cfaeda86720f06fc205c763d935b7659147a97
elbee <66045908+elbee-cyber@users.noreply.github.com>
2025-05-12 14:34:20 -0400

    stuff

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo 'deb http://archive.debian.org/debian-security stretch/updates main contrib
 sudo apt update
 # Apps and base system
 sudo apt install polybar i3 rofi zsh python3-pip curl autoconf build-essential git stow tmux tree vim-nox picom -y
-sudo pip3 install pywal
+sudo pip3 install pywal --break-system-packages
 
 echo "Installing networkmanager-dmenu..."
 git clone https://github.com/firecat53/networkmanager-dmenu
@@ -28,18 +28,33 @@ fc-cache -f -v
 echo "You will be prompted to generate i3 config, press return"
 i3-config-wizard
 
+echo "Backing up i3 config..."
+mkdir -p ~/.config/i3/backup
+cp -rf ~/.config/i3/config ~/.config/i3/backup/config.bak
+
+echo "Configuring alacritty..."
+sudo alacritty migrate
+sudo chown `whoami`:`whoami` /etc/xdg/alacritty/alacritty.toml
+echo "set \$term alacritty" >>~/.config/i3/config
+sed -i -E 's|(^\s*bindsym\s+\$mod\+Return\s+exec\s+).*|\1alacritty|' ~/.config/i3/config
+
 echo "Configuring polybar..."
 mkdir -p ~/.config/polybar/forest
 cp -rf config/polybar/* ~/.config/polybar/forest
 chmod +x ~/.config/polybar/forest/launch.sh
 echo "exec_always --no-startup-id ~/.config/polybar/launch.sh" >>~/.config/i3/config
 
-echo "Configuring picom..."
+echo "Configuring picom borders..."
 mkdir -p ~/.config/picom
 cp -rf config/picom/* ~/.config/picom
 echo "picom --config ~/.config/picom/picom.conf &" >>~/.config/i3/config
 
 echo "Adding gaps to i3..."
-echo "gaps inner 10" >>~/.config/i3/config
-echo "gaps outer 10" >>~/.config/i3/config
+echo "gaps inner 15" >>~/.config/i3/config
+echo "gaps outer 15" >>~/.config/i3/config
 echo "smart_borders on" >>~/.config/i3/config
+
+echo "Setting wallpaper..."
+mkdir -p ~/.config/wallpapers
+cp dark-sky.jpg ~/.config/wallpapers
+echo "wal -i ~/.config/wallpapers/dark-sky.jpg" >>~/.config/i3/config
