07f6c09d2588fe3ff1a2f44089d8a148eff001d7
elbee <66045908+elbee-cyber@users.noreply.github.com>
2025-05-12 13:36:43 -0400

    Add scripts and configuration for Polybar integration

--- install.sh
+++ install.sh
@@ -2,35 +2,17 @@
 # Polybar themes: https://github.com/adi1090x/polybar-themes (alot was taken from here)
 
 echo "Installing packages..."
-echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
+echo 'deb http://archive.debian.org/debian-security stretch/updates main contrib non-free
+' | sudo tee -a /etc/apt/sources.list.d/backports.list
 sudo apt update
 # Apps and base system
-sudo apt install polybar kitty rofi zsh python3-pip curl python3-pywal autoconf build-essential git stow tmux tree vim-nox
-# i3-gaps dependencies
-sudo apt install libev-dev libpango1.0-dev libstartup-notification0-dev libxcb-cursor-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xinerama0-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-xrm0 libxcb1-dev libxkbcommon-dev libxkbcommon-x11-dev libyajl-dev xcb
+sudo apt install polybar i3 rofi zsh python3-pip curl autoconf build-essential git stow tmux tree vim-nox picom -y
+sudo pip3 install pywal
 
 echo "Installing networkmanager-dmenu..."
 git clone https://github.com/firecat53/networkmanager-dmenu
-sudo cp ./networkmanager-dmenu/networkmanager-dmenu /usr/local/bin/networkmanager-dmenu
-/usr/local/bin/networkmanager-dmenu
+sudo cp networkmanager-dmenu/networkmanager_dmenu /usr/local/bin/networkmanager-dmenu
 rm ./networkmanager-dmenu -rf
-
-echo "Installing i3-radius..."
-cd /usr/local/src
-git clone https://github.com/Airblader/i3 i3-radius
-cd i3-radius
-hash=$(git rev-parse --short HEAD)
-autoreconf --force --install
-mkdir build && cd "$_"
-../configure --prefix=/usr/local --sysconfdir=/etc --disable-sanitizers
-make PREFIX=/usr/local
-make install prefix=/usr/local/stow/i3-radius-$(hash)
-find /usr/local/stow/i3-radius-$(hash) -type d -exec chmod 755 {} \;
-find /usr/local/stow/i3-radius-$(hash) -type f -exec chmod 644 {} \;
-chmod -R 755 /usr/local/stow/i3-radius-$(hash)/bin
-cd /usr/local/stow
-stow i3-radius-$(hash)
-sudo apt install -t stretch-backports feh i3blocks i3lock i3status lightdm sakura suckless-tools xorg
 
 echo "Installing powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
@@ -40,4 +22,24 @@ echo 'source ~/.config/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo "Installing fonts..."
 FDIR="$HOME/.local/share/fonts"
 [[ ! -d "$FDIR" ]] && mkdir -p "$FDIR"
-cp -rf `pwd`/fonts/* "$FDIR"
+cp -rf fonts/* "$FDIR"
+fc-cache -f -v
+
+echo "You will be prompted to generate i3 config, press return"
+i3-config-wizard
+
+echo "Configuring polybar..."
+mkdir -p ~/.config/polybar/forest
+cp -rf config/polybar/* ~/.config/polybar/forest
+chmod +x ~/.config/polybar/forest/launch.sh
+echo "exec_always --no-startup-id ~/.config/polybar/launch.sh" >>~/.config/i3/config
+
+echo "Configuring picom..."
+mkdir -p ~/.config/picom
+cp -rf config/picom/* ~/.config/picom
+echo "picom --config ~/.config/picom/picom.conf &" >>~/.config/i3/config
+
+echo "Adding gaps to i3..."
+echo "gaps inner 10" >>~/.config/i3/config
+echo "gaps outer 10" >>~/.config/i3/config
+echo "smart_borders on" >>~/.config/i3/config
